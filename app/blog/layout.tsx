a9aca9520d8f818c36ec0b766a3e97066a1f9605
Thor <43585332+mua105310@users.noreply.github.com>
2024-12-28 03:11:07 +0100

    adding footer

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,8 +1,13 @@
+import Footer from "@/components/Footer"
+
 export default function BlogLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <div className="min-h-screen bg-gradient-to-br from-black via-black to-[#1a1a3a]">{children}</div>
+  return <div className="min-h-screen bg-gradient-to-br from-black via-black to-[#1a1a3a]">
+    {children}
+    <Footer></Footer>
+    </div>
 }
 
